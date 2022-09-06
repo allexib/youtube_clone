2f0380f5976e385eb3f87c8c61025f00f2421dca
allexib <ooomegasib@gmail.com>
2022-09-06 15:00:29 +0300

    videoCard

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -10,15 +10,16 @@ import {
   demoChannelUrl,
   demoChannelTitle,
 } from "../utils/constants";
+import { CheckCircle } from "@mui/icons-material";
 
-const VideoCard= ({
+const VideoCard = ({
   video: {
     id: { videoId },
     snippet,
   },
 }) => {
   return (
-    <Card>
+    <Card sx={{ width: { md: "320px", xs: "100%" }, boxShadow: "none", borderRadius: "none" }}>
       <Link to={videoId ? `/video${videoId}` : demoVideoUrl}>
         <CardMedia
           image={snippet?.thumbnails?.high?.url}
@@ -26,6 +27,19 @@ const VideoCard= ({
           sx={{ width: { xs: "100%", sm: "358px" }, height: 180 }}
         />
       </Link>
+      <CardContent sx={{ backgroundColor: "#1E1E1E", height: "106px" }}>
+        <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+          <Typography variant="subtitle1" fontWeight="bold" color="#FFF">
+            {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
+          </Typography>
+        </Link>
+        <Link to={snippet?.channelId ? `/channel/${snippet?.channelId}` : demoChannelUrl}>
+          <Typography variant="subtitle2" fontWeight="bold" color="gray">
+            {snippet?.channelTitle || demoChannelTitle}
+            <CheckCircle sx={{ fontSize: 12, color: "gray", ml: 5 }} />
+          </Typography>
+        </Link>
+      </CardContent>
     </Card>
   );
 };
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -16,7 +16,7 @@ export const logo = "https://i.ibb.co/s9Qys2j/logo.png";
 
 export const categories = [
   { name: "New", icon: <HomeIcon /> },
-  { name: "Allexib", icon: <CodeIcon /> },
+  { name: "Rezilienza", icon: <CodeIcon /> },
   { name: "Coding", icon: <CodeIcon /> },
   { name: "ReactJS", icon: <CodeIcon /> },
   { name: "NextJS", icon: <CodeIcon /> },
