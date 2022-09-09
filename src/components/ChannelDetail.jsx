400064e5ee6f8d72179c34403f7c05c8630ab3f9
allexib <ooomegasib@gmail.com>
2022-09-09 18:39:07 +0300

    channelDetail, reuse component

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -6,12 +6,12 @@ import { Videos, ChannelCard } from "./";
 import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const ChannelDetail = () => {
-  const [channelDeatail, setChannelDeatail] = useState(null);
+  const [channelDetail, setChannelDeatail] = useState(null);
   const [videos, setvideos] = useState([]);
 
   const { id } = useParams();
 
-  console.log(channelDeatail, videos);
+  // console.log(channelDeatail, videos);
 
   useEffect(() => {
     fetchFromAPI(`channels?part=snippet&id=${id}`).then((data) => setChannelDeatail(data?.items[0]));
@@ -19,7 +19,21 @@ const ChannelDetail = () => {
     fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`).then((data) => setvideos(data?.items));
   }, [id]);
 
-  return <div>{id}</div>;
+  return (
+    <Box minHeight="95vh">
+      <Box>
+        <div
+          style={{
+            background:
+              "linear-gradient(90deg, rgba(244,128,249,1) 0%, rgba(16,27,125,1) 59%, rgba(0,212,255,1) 100%)",
+            zIndex: 10,
+            height: "300px",
+          }}
+        />
+        <ChannelCard channelDetail={channelDetail} />
+      </Box>
+    </Box>
+  );
 };
 
 export default ChannelDetail;
