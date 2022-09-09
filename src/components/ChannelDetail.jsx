b8e292bc6253547712ce5c41729766607e0d1246
allexib <ooomegasib@gmail.com>
2022-09-09 17:59:31 +0300

    channelDetail, fetch videos, data

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -6,7 +6,18 @@ import { Videos, ChannelCard } from "./";
 import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const ChannelDetail = () => {
+  const [channelDeatail, setChannelDeatail] = useState(null);
+  const [videos, setvideos] = useState([]);
+
   const { id } = useParams();
+
+  console.log(channelDeatail, videos);
+
+  useEffect(() => {
+    fetchFromAPI(`channels?part=snippet&id=${id}`).then((data) => setChannelDeatail(data?.items[0]));
+
+    fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`).then((data) => setvideos(data?.items));
+  }, [id]);
 
   return <div>{id}</div>;
 };
