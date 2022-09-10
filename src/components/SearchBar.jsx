19cefc1c766f4e9d5b6c6e35355e2832e51b849f
allexib <ooomegasib@gmail.com>
2022-09-10 14:46:04 +0300

    serchBar, setSearchTerm, full work search

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,13 +1,27 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Paper, IconButton } from "@mui/material";
-import {Search} from "@mui/icons-material/Search";
+import { Search } from "@mui/icons-material/Search";
 
 const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (searchTerm) {
+      navigate(`/search/${searchTerm}`);
+
+      setSearchTerm("");
+    }
+  };
+
   return (
     <Paper
       component="form"
-      // onSubmit={onhandleSubmit}
+      onSubmit={handleSubmit}
       sx={{
         borderRadius: 20,
         border: "1px solid #e3e3e3",
@@ -19,8 +33,8 @@ const SearchBar = () => {
       <input
         className="search-bar"
         placeholder="Search..."
-        // value={searchTerm}
-        // onChange={(e) => setSearchTerm(e.target.value)}
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
       />
       <IconButton type="submit" sx={{ p: "10px", color: "red" }} aria-label="search">
         {/* <SearchIcon /> */}
